b7942be5b6da5443262cafa694971cfb49cc21bf
Naivys Tirado Ruiz - 10129073 <naivys.tiradoruiz1@fiserv.com>
2024-07-31 10:53:03 -0400

    Added method for generating dates

--- dates-util/src/main/java/org/example/DatesUtil.java
+++ dates-util/src/main/java/org/example/DatesUtil.java
@@ -1,12 +1,26 @@
 package org.example;
 
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatesUtil {
 
-    private DatesUtil(){}
+    private DatesUtil() {
+    }
 
-    public static void generateDates(String start, String end){
+    public static void generateDates(String startTime, String endTime) {
+        int secondsInterval = 5;
+        Date startDateTime = Timestamp.valueOf(startTime);
+        Date endDateTime = Timestamp.valueOf(endTime);
 
+        while (startDateTime.getTime() < endDateTime.getTime()) {
+            Date endQueryDateTime = DateUtils.addSeconds(startDateTime, secondsInterval);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            System.out.println(sdf.format(startDateTime) + "," + sdf.format(endQueryDateTime));
+            startDateTime = endQueryDateTime;
+        }
     }
 }
--- dates-util/src/main/java/org/example/Main.java
+++ dates-util/src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        String startTime = "2024-07-29 10:00:00.000";
+        String endTime = "2024-07-29 11:00:00.000";
+        DatesUtil.generateDates(startTime, endTime);
     }
 
 
