b5a468ddef72ccf94dc6fb91350949a2cd366247
Naivys Tirado Ruiz - 10129073 <naivys.tiradoruiz1@fiserv.com>
2024-07-31 10:42:00 -0400

    Initial commit

---
+++ dates-util/src/main/java/org/example/DatesUtil.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Date;
+
+public class DatesUtil {
+
+    private DatesUtil(){}
+
+    public static void generateDates(String start, String end){
+
+    }
+}
